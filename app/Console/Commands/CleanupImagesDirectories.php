64df0d8e1538755129ab2ea27abf9990ab203f2f
teabubble <kobsyy@gmail.com>
2025-10-18 08:39:32 +0200

    test upload

---
+++ app/Console/Commands/CleanupImagesDirectories.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CleanupImagesDirectories extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:cleanup-images-directories';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
